00a001199dfafb572e484c0be40e591b0ab545b6
Jacob Masen-Smith <smith.jacob@gmail.com>
2018-02-15 14:16:19 -0800

    added windivert header to decode

--- src/decode.h
+++ src/decode.h
@@ -64,6 +64,7 @@ enum PktSrcEnum {
 #include "source-af-packet.h"
 #include "source-mpipe.h"
 #include "source-netmap.h"
+#include "source-windivert.h"
 #ifdef HAVE_PF_RING_FLOW_OFFLOAD
 #include "source-pfring.h"
 #endif
